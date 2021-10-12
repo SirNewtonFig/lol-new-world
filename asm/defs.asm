ce140157b3e92dbbc10e142149887820793ce705
SirNewtonFig <sirnewtonfig@gmail.com>
2021-10-11 21:57:22 -0300

    Make dog block chance based on Stamina instead of flat 50%

--- asm/defs.asm
+++ asm/defs.asm
@@ -1,9 +1,10 @@
 !Automorph_freeC2 = $C2FC10     ; 7 bytes free; we'll use 5
+!TieredStatus_freeC2 = $C2515D  ; 4 bytes
+!StamDogBlock_freeC2 = $C23C6E  ; 7 bytes; we'll use 4
+
 !Automorph_freeXX = $EFFD7A
-
+!TieredStatus_freeXX = $EFFBDA
+!StamDogBlock_freeXX = $EFFDC2
 !HalfTurnBrushes_freespace = $EFFDA5
-
 !HalfTurnStumble_freespace = $EFFDB5
 
-!TieredStatus_freeXX = $EFFBDA
-!TieredStatus_freeC2 = $C2515D  ; 4 bytes
