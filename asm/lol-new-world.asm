1a4202b9f5ddf1bef18801eb5f0cb30d62843d57
SirNewtonFig <sirnewtonfig@gmail.com>
2021-10-17 23:34:22 -0300

    Add counterattack chance after successful Runic

--- asm/lol-new-world.asm
+++ asm/lol-new-world.asm
@@ -13,8 +13,9 @@ incsrc "patches/tiered-status.asm"
 incsrc "patches/el-tweaks.asm"
 incsrc "patches/stam-dog-block.asm"
 incsrc "patches/vigor-based-counter.asm"
+incsrc "patches/runic-counter.asm"
 incsrc "patches/data/dance-descriptions.asm"
 
 ; on loan from Bropedio; I haven't scrutinized these, just assuming they're going to work as advertised
-incsrc "patches/Bropedio/golem-restrictions.asm"
+incsrc "patches/Bropedio/golem-restrictions.asm" ; omit when building against 2.1
 incsrc "patches/Bropedio/inventory-limits.asm"
