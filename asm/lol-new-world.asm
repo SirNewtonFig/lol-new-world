d79ac397cc24e3aed83b237a8618f1a625fb991b
SirNewtonFig <sirnewtonfig@gmail.com>
2021-10-24 10:52:53 -0300

    Let both Relm and Shadow have doggo, even if Shadow alive

--- asm/lol-new-world.asm
+++ asm/lol-new-world.asm
@@ -19,7 +19,11 @@ incsrc "patches/crit-fishing.asm"
 incsrc "patches/not-so-near-fatal.asm"
 incsrc "patches/half-atb-blitz-failure.asm"
 
+; data/description edits
 incsrc "patches/data/dance-descriptions.asm"
+
+; event edits
+incsrc "patches/events/relm-interceptor.asm"
 
 ; on loan from Bropedio; I haven't scrutinized these, just assuming they're going to work as advertised
 incsrc "patches/Bropedio/golem-restrictions.asm" ; omit when building against 2.1
