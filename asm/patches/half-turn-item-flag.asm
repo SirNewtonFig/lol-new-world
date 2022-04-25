20e5a37538072c5c573f69536e94889d1b2d54e3
SirNewtonFig <sirnewtonfig@gmail.com>
2022-04-24 21:50:59 -0300

    Remove traces of Economizer code from menu

--- asm/patches/half-turn-item-flag.asm
+++ asm/patches/half-turn-item-flag.asm
@@ -1,9 +1,21 @@
+; Menu Economizer logic
+
+org $C3512E           ; Menu Economizer check
+BRA Hairpin           ; proceed without doing Economizer check
+
+org $C3513A           ; Gold Hairpin check
+Hairpin:              ; convenience label for above
+
+; Battle Economizer logic
+
 org $C25741           ; MP modification routine
   XBA                 ; flip back to MP cost
   RTS                 ; return without doing Economizer check
 
 padbyte $FF
 pad $C2574B           ; Erase Economizer code
+
+; New hotness: Half-ATB Item
 
 org $C21897           ; Item command
   JSL ItemSlice
