0df2198c51cb265b50daf3444a6b6f3a2b48022a
Greg Murrell <j_greg_m@comcast.net>
2022-05-10 11:43:20 -0400

    speed based counter rate

--- asm/patches/half-atb-sketch-failure.asm
+++ asm/patches/half-atb-sketch-failure.asm
@@ -4,8 +4,8 @@ org $C23B23				; where Sketch and Control are setting "missed"
 	
 org !HalfTurnSketch_freespace
 SketchSlice:
-	TSB $3A5A			
-	TRB $A4
+	TSB $3A5A			; (Set target as missed)
+	TRB $A4				; (Miss target)
 	JSR HalfATBY		; set ATB to 50%
 	RTL
 	
