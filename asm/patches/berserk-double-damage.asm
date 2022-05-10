ddebb79fb663e558fb4be5d259fef93d8f0db9a8
Greg Murrell <j_greg_m@comcast.net>
2022-05-10 17:03:45 -0400

    berserk multiplier is double damage

---
+++ asm/patches/berserk-double-damage.asm
@@ -0,0 +1,18 @@
+; needs to abort on Umaro and Gau
+; The other thing you could do is put a little slice in where the berserk multiplier gets applied, and do a little if/elsif/else branch that checks for Rage status or Umaro actor ID and just leaves the multiplier alone. So they'd get zerked, but it does nothing useful for them.
+; alternatively, just have Clubs grant Berserk immunity
+; Umaro id = $300D
+; Gau id = $300B
+; $3A93 =? Rage
+
+org $C2337C				; where Berserk's multiplier is found
+	JSL BerserkDoubleMultiplier
+	NOP
+	
+org !BerserkDoubleMultiplier_freespace
+BerserkDoubleMultiplier:
+	INC $BC			; (Add 50% damage if berserk and physical attack)
+	INC $BC			; (Add 50% damage if berserk and physical attack)
+	RTL
+	
+BerserkDoubleMultiplier_EOF:
