5cf72bd0a73cdda66d244a507822b24d47b402e9
Greg Murrell <j_greg_m@comcast.net>
2022-07-30 18:15:03 -0400

    revert Stray back to stam++

--- asm/patches/el-tweaks.asm
+++ asm/patches/el-tweaks.asm
@@ -19,20 +19,20 @@ hirom
 
 org $C2614E
 ELTable:
-  dw $3C3C ; $00 - 40HP - Terrato, Crusader
-  dw $3E3E ; $01 - 40MP - Ragnarok
-  dw $3C3E ; $02 - 20HP/20MP -  Phoenix
-  dw $013C ; $03 - 20HP/Vig - Golem
-  dw $3E07 ; $04 - 20MP/Mag - Carbunkl
-  dw $0103 ; $05 - Vig/Spd - Palidor
+  dw $3C3C ; $00 - HP++ - Terrato, Crusader
+  dw $3E3E ; $01 - MP++ - Ragnarok
+  dw $3C3E ; $02 - HP/MP -  Phoenix, Seraph
+  dw $013C ; $03 - HP/Vig - Golem
+  dw $3E07 ; $04 - MP/Mag - Carbunkl
+  dw $0103 ; $05 - Vig/Spd - Palidor, Alexander
   dw $0307 ; $06 - Mag/Spd - Siren
   dw $0105 ; $07 - Vig/Stm - Phantom
-  dw $013E ; $08 - Vig/20MP - Tritoch
-  dw $073C ; $09 - 20HP/Mag - Alexander
-  dw $3C05 ; $0A - 20HP/Stm - Kirin, Unicorn, Stray
-  dw $053E ; $0B - 20MP/Stm - Zoneseek, Seraph
-  dw $0101 ; $0C - 2Vig - Ramuh, Bismark
-  dw $0303 ; $0D - 2Spd - Ifrit, Fenrir
-  dw $0505 ; $0E - 2Stm - Starlet, Maduin
-  dw $0707 ; $0F - 2Mag - Shiva, Shoat, Bahamut
-  dw $0305 ; $10 - Spd/Stam - Odin
+  dw $013E ; $08 - Vig/MP - Tritoch
+  dw $0305 ; $09 - Spd/Stm - Odin
+  dw $3C05 ; $0A - HP/Stm - Kirin, Unicorn, Stray
+  dw $053E ; $0B - MP/Stm - Zoneseek
+  dw $0101 ; $0C - Vig++ - Ramuh, Bismark
+  dw $0303 ; $0D - Spd++ - Ifrit, Fenrir
+  dw $0505 ; $0E - Stm++ - Starlet, Maduin
+  dw $0707 ; $0F - Mag++ - Shiva, Shoat, Bahamut
+  dw $0000 ; $10 - free - n/a
