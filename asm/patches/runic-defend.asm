6db84d29b9f930ef4b45bbebe3587ea77234c1ef
SirNewtonFig <sirnewtonfig@gmail.com>
2022-09-04 22:59:57 -0300

    Fix defend/weapon swap indexing error

--- asm/patches/runic-defend.asm
+++ asm/patches/runic-defend.asm
@@ -1,34 +1,38 @@
-org $C219DD       ; Command pointer table entries
+org $C18B9A
+SwapOrDefend:       ; Convenience label
+
+org $C219DD         ; Command pointer table entries
   dw Runic
 org $C219EF
   dw Row
 org $C219F1
   dw Defend
 
-org $C2194C       ; Start of Row command
+org $C2194C         ; Start of Row command
 
 Row:
   TYX
   LDA $3AA1,X
   EOR #$20
-  STA $3AA1,X    ; Toggle Row
+  STA $3AA1,X       ; Toggle Row
   BRA ATBRefund
   
 Runic:
   TYX
   LDA $3E4C,X
   ORA #$04
-  STA $3E4C,X    ; Set Runic
+  STA $3E4C,X       ; Set Runic
   LDA #$02
-  JSR $5BAB      ; set Defending flag
-  BRA WrapUp
+  JSR $5BAB         ; set Defending flag
+  BRA SelfHit
 
 Defend:
   TYX
-  JSL $C18B9A    ; Load Defending flag (weapon swap stuff also goes through here)
-  JSR $5BAB      ; set Defending flag
+  JSL SwapOrDefend  ; Load Defending flag (weapon swap stuff also goes through here)
+  JSR $5BAB         ; set Defending flag
+  TYX
 ATBRefund:
-  JSL $C18A0E    ; Refund half ATB
-WrapUp:
-  JSR $298A      ; Clear special effect, magic power, etc.
+  JSL $C18A0E       ; Refund half ATB
+SelfHit:
+  JSR $298A         ; Clear special effect, magic power, etc.
   JMP $3167
